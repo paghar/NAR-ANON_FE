89c58c65c51a602249cffd0dfde9c3ee6af719f0
Stephan Sperling <sperling.stephan@googlemail.com>
2023-02-20 20:37:44 +0100

    :art: insert the theme to our project for rework

---
+++ src/components/misc/ButtonPrimary.tsx
@@ -0,0 +1,14 @@
+const ButtonPrimary = ({ children, addClass }: any) => {
+  return (
+    <button
+      className={
+        "py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-orange-500 hover:shadow-orange-md transition-all outline-none " +
+        addClass
+      }
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ButtonPrimary;
