dc5b8ec96decc48d482345a897e309d75e3a8081
Fatemeh Paghar <f.paghar@gmail.com>
2023-02-20 16:05:16 +0100

    Feature Adding them

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./pages/*.js",
+      "./pages/**/*.js",
+      "./components/*.js",
+      "./components/**/*.js",
+    ],
+    whitelistPatterns: [/^slick-/],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
-}
+];
+module.exports = {
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
