862746195d563124815cd6e10f3ba6ce5cb9d87f
Fatemeh Paghar <f.paghar@gmail.com>
2023-02-24 13:13:15 +0100

    add image gallary

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,12 @@ import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <>
+    <>   
       <SeoHead title="Iranian commuity" />
-      <Layout>
-        <Hero/>
-        <Gallary/>
-        <Planes/>
+        <Layout>
+          <Hero/>
+          <Gallary/>
+          <Planes/>
       </Layout>
     </>
   );
