a51dfb37eac3bf742a91eb85519f01529f07c2c9
Fatemeh Paghar <f.paghar@gmail.com>
2023-04-01 13:23:50 +0430

    add internationalization and rtl layout support

--- src/components/SeoHead.tsx
+++ src/components/SeoHead.tsx
@@ -1,102 +1,5 @@
 import Head from "next/head";
-import {useRouter} from "next/router";
-
-
-// Default value for some meta data
-const defaultMeta = {
-  title: "LaslesVPN",
-  siteName: "LaslesVPN",
-  description:
-    "Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest",
-  // change base url of your web (without '/' at the end)
-  url: "https://next-landing-vpn.vercel.app",
-  type: "website",
-  robots: "follow, index",
-  // change with url of your image (recommended dimension = 1.91:1)
-  // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: "https://next-landing-vpn.vercel.app/assets/card-image.png",
-  author: "Lorem Ipsum"
-};
-
-/**
- * Next Head component populated with necessary SEO tags and title
- * props field used:
- * - title
- * - siteName
- * - description
- * - url
- * - type
- * - robots
- * - image
- * - date
- * - author
- * - templateTitle
- * all field are optional (default value defined on defaultMeta)
- * @example
- * <SeoHead title="Page's Title" />
- */
-const SeoHead = (props) => {
-  const router = useRouter();
-  const meta = {
-    ...defaultMeta,
-    ...props
-  };
-
-  // Use siteName if there is templateTitle
-  // but show full title if there is none
-  meta.title = props.templateTitle
-    ? `${props.templateTitle} | ${meta.siteName}`
-    : meta.title;
-
-  return (
-    <Head>
-      <title>{meta.title}</title>
-      <meta name='robots' content={meta.robots} />
-      <meta content={meta.description} name='description' />
-      <meta property='og:url' content={`${meta.url}${router.asPath}`} />
-      <link rel='canonical' href={`${meta.url}${router.asPath}`} />
-      {/* Open Graph */}
-      <meta property='og:type' content={meta.type} />
-      <meta property='og:site_name' content={meta.siteName} />
-      <meta property='og:description' content={meta.description} />
-      <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content={meta.image} />
-      {/* Twitter */}
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:site' content='@F2aldi' />
-      <meta name='twitter:title' content={meta.title} />
-      <meta name='twitter:description' content={meta.description} />
-      <meta name='twitter:image' content={meta.image} />
-      {meta.date && (
-        <>
-          <meta property='article:published_time' content={meta.date} />
-          <meta
-            name='publish_date'
-            property='og:publish_date'
-            content={meta.date}
-          />
-          <meta
-            name='author'
-            property='article:author'
-            content={meta.author}
-          />
-        </>
-      )}
-      {/* Favicons */}
-      {favicons.map((linkProps) => (
-        <link key={linkProps.href} {...linkProps} />
-      ))}
-      {/* Windows 8 app icon */}
-      <meta name='msapplication-TileColor' content='#F53838' />
-      <meta
-        name='msapplication-TileImage'
-        content='/favicon/ms-icon-144x144.png'
-      />
-      {/* Accent color on supported browser */}
-      <meta name='theme-color' content='#F53838' />
-    </Head>
-  );
-};
+import { useRouter } from "next/router";
 
 // Favicons, other icons, and manifest definition
 const favicons = [
@@ -183,5 +86,87 @@ const favicons = [
     href: "/site.webmanifest",
   },
 ];
+
+// Default value for some meta data
+const defaultMeta = {
+  title: "LaslesVPN",
+  siteName: "LaslesVPN",
+  description: "Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest",
+  // change base url of your web (without '/' at the end)
+  url: "https://next-landing-vpn.vercel.app",
+  type: "website",
+  robots: "follow, index",
+  // change with url of your image (recommended dimension = 1.91:1)
+  // used in twitter, facebook, etc. card when link copied in tweet/status
+  image: "https://next-landing-vpn.vercel.app/assets/card-image.png",
+  author: "Lorem Ipsum",
+};
+
+/**
+ * Next Head component populated with necessary SEO tags and title
+ * props field used:
+ * - title
+ * - siteName
+ * - description
+ * - url
+ * - type
+ * - robots
+ * - image
+ * - date
+ * - author
+ * - templateTitle
+ * all field are optional (default value defined on defaultMeta)
+ * @example
+ * <SeoHead title="Page's Title" />
+ */
+const SeoHead = (props: any) => {
+  const router = useRouter();
+  const meta = {
+    ...defaultMeta,
+    ...props,
+  };
+
+  // Use siteName if there is templateTitle
+  // but show full title if there is none
+  meta.title = props.templateTitle ? `${props.templateTitle} | ${meta.siteName}` : meta.title;
+
+  return (
+    <Head>
+      <title>{meta.title}</title>
+      <meta name="robots" content={meta.robots} />
+      <meta content={meta.description} name="description" />
+      <meta property="og:url" content={`${meta.url}${router.asPath}`} />
+      <link rel="canonical" href={`${meta.url}${router.asPath}`} />
+      {/* Open Graph */}
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={meta.siteName} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+      <meta name="image" property="og:image" content={meta.image} />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@F2aldi" />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+      <meta name="twitter:image" content={meta.image} />
+      {meta.date && (
+        <>
+          <meta property="article:published_time" content={meta.date} />
+          <meta name="publish_date" property="og:publish_date" content={meta.date} />
+          <meta name="author" property="article:author" content={meta.author} />
+        </>
+      )}
+      {/* Favicons */}
+      {favicons.map((linkProps) => (
+        <link key={linkProps.href} {...linkProps} />
+      ))}
+      {/* Windows 8 app icon */}
+      <meta name="msapplication-TileColor" content="#F53838" />
+      <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
+      {/* Accent color on supported browser */}
+      <meta name="theme-color" content="#F53838" />
+    </Head>
+  );
+};
 
 export default SeoHead;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,11 @@
-import {Html, Head, Main, NextScript} from "next/document";
+import { Html, Head, Main, NextScript, DocumentProps } from "next/document";
 
-export default function Document() {
+export default function Document(props: DocumentProps) {
+  const locale = props.__NEXT_DATA__.locale;
+  const dir = locale === "fa" ? "rtl" : "ltr";
+
   return (
-    <Html lang="en">
+    <Html lang={locale} dir={dir}>
       <Head />
       <body>
         <Main />
