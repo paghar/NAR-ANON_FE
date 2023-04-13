8ad1f66dec036709aa1174b0d9e853b45a8aa319
Fatemeh Paghar <f.paghar@gmail.com>
2023-04-13 20:12:20 +0200

    change home page gallary style

--- src/pages/gallary.tsx
+++ src/pages/gallary.tsx
@@ -4,8 +4,8 @@ import {GetStaticPropsContext} from "next";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 
 import ButtonOutline from "@/components/misc/ButtonOutline";
-
 import PhotoAlbum from "react-photo-album";
+
 import photos from "@/components/photos";
 import Lightbox from "yet-another-react-lightbox";
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
@@ -33,7 +33,7 @@ const gallary = () =>{
   return(
     <div className="flex max-w-screen-xl mt-36 mb-4 px-8 xl:px-16 mx-auto">
 
-      <div className="flex flex-col w-auto mr-4">
+      <div className="flex flex-col w-auto mx-4">
         <ButtonOutline id="item1" type="button" addClass="mb-2" onClick={()=>null}>item1</ButtonOutline>
         <ButtonOutline id="item1" type="button" addClass="mb-2" onClick={()=>null}>item2</ButtonOutline>
         <ButtonOutline id="item1" type="button" addClass="mb-2" onClick={()=>null}>item3</ButtonOutline>            
